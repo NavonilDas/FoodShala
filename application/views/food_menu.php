3eab452ddd955a68ab581085ff933d970abd44c4
Navonil Das <navneeladas@gmail.com>
2021-01-26 13:26:57 +0530

    Food Menu Navbar

---
+++ application/views/food_menu.php
@@ -0,0 +1,85 @@
+<?php
+	$user = $this->session->userdata( 'user' );
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>FoodShala</title>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script> -->
+	</head>
+<body>
+	<style>
+		.avatar{
+			width:40px;
+			height:40px;
+			border-radius:50%;
+			background-color:#f6f6f6;
+			position:relative;
+		}
+	</style>
+
+	<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+	<a class="navbar-brand" href="<?php echo base_url(); ?>">FoodShala</a>
+
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		<span class="navbar-toggler-icon"></span>
+	</button>
+
+	<div class="collapse navbar-collapse" id="navbarSupportedContent">
+		<ul class="navbar-nav mr-auto">
+			<li class="nav-item active">
+				<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="#">Link</a>
+			</li>
+		</ul>
+		<form class="form-inline my-2 my-lg-0">
+		<!-- <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"> -->
+		<?php if ( $user === null ) { ?>
+			<button class="btn btn-light my-2 my-sm-0 login" type="button"><i class="fa fa-user"></i> Login</button>
+			<button class="btn btn-light my-2 my-sm-0 ml-2 signup" type="button"><i class="fa fa-user-plus"></i> Sign Up</button>
+		<?php } else { ?>
+			<button class="btn btn-light my-2 my-sm-0 cart" type="submit"><i class="fa fa-shopping-cart"></i> Cart</button>
+			
+			<div class="avatar ml-2 d-flex" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+				<span class="m-auto"><?php echo $user->name[0]; ?></span>
+
+				<div class="dropdown-menu" aria-labelledby="navbarDropdown" style="left:-150px">
+					<a class="dropdown-item" href="#"><?php echo $user->name; ?></a>
+					<div class="dropdown-divider"></div>
+					<a class="dropdown-item" href="#">Logout</a>
+				</div>
+
+			</div>
+		
+
+		<?php } ?>
+		</form>
+	</div>
+	</nav>
+
+
+	<div class="row">
+
+	</div>
+
+	<script>
+	$('.login').click(function() {
+		window.location.href = "<?php echo base_url() . 'login'; ?>"
+	});
+	$('.signup').click(function() {
+		window.location.href = "<?php echo base_url() . 'register'; ?>"
+	});
+	$('.cart').click(function() {
+		window.location.href = "<?php echo base_url() . 'cart'; ?>"
+	});
+	</script>
+</body>
+</html>
