05daf2d100843b98c2ecfb4c7c11e3e354dad440
Navonil Das <navneeladas@gmail.com>
2021-01-28 17:39:30 +0530

    Comments on Menu & Login Controller

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -1,43 +1,75 @@
 <?php
 defined( 'BASEPATH' ) or exit( 'No direct script access allowed' );
 
+/**
+ * Login Controller Class: For the /login route
+ *
+ * @version 0.1.0
+ * @author Navonil Das
+ */
 class Login extends CI_Controller {
 
+	/**
+	 * Controller For Login Form.
+	 *
+	 * @return void
+	 */
 	public function index() {
+		// Load Form Validation Library
 		$this->load->library( 'form_validation' );
 
-		// Rules
+		// Rules For Form Validation
 		$this->form_validation->set_rules( 'email', 'Email', 'required|valid_email' );
 		$this->form_validation->set_rules( 'password', 'Password', 'required' );
 
+		// if validation failed show the login page
 		if ( $this->form_validation->run() == false ) {
 			$this->load->view( 'login' );
 		} else {
+			// Load User Auth Model
 			$this->load->model( 'UserAuth' );
+
+			// Get Email & Password
 			$email = $this->input->post( 'email' );
 			$pass  = $this->input->post( 'password' );
+			
+			// Get User Using email & pass
 			$user  = $this->UserAuth->login( $email, $pass );
+
+			// if email or pass is wrong ie. User is null the show Error message on the Login form.
 			if ( $user == null ) {
 				$data          = array();
 				$data['error'] = 'Invalid Email or Password';
+				// View Login Form
 				$this->load->view( 'login', $data );
 			} else {
+				// Get User Role from id
 				$role = $this->UserAuth->getUserRole( $user->id );
-
+				
+				// Save User & role on session
 				$this->session->set_userdata(
 					array(
 						'user' => $user,
 						'role' => ( $role === null ) ? 'anonymous' : $role->type,
 					)
 				);
+
+				// redirect to home page
 				redirect( '/' );
 			}
 		}
 	}
 
+	/**
+	 * Controller to logout user.
+	 *
+	 * @return void
+	 */
 	public function logout() {
+		// Clear Session
 		$this->session->unset_userdata( 'user' );
 		$this->session->unset_userdata( 'role' );
+		// Redirect to home page.
 		redirect( '/' );
 	}
 
--- application/controllers/Menu.php
+++ application/controllers/Menu.php
@@ -2,7 +2,7 @@
 defined( 'BASEPATH' ) or exit( 'No direct script access allowed' );
 
 /**
- * Menu Model Class: For the food_menu table
+ * Menu Controller Class: For the /menu route
  *
  * @version 0.1.0
  * @author Navonil Das
@@ -10,81 +10,118 @@ defined( 'BASEPATH' ) or exit( 'No direct script access allowed' );
 class Menu extends CI_Controller {
 
 	/**
+	 * Controller For Add Food menu item form.
+	 *
+	 * @return void
 	 */
 	public function index() {
+		// Load User Model
 		$this->load->model( 'UserAuth' );
+		// Load Preference Model
 		$this->load->model( 'PreferenceModel' );
 
+		// Load user & role from session.
 		$user = $this->session->userdata( 'user' );
+		$role = $this->session->userdata( 'role' );
 
-		if ( $user === null ) {
+		// if user is not Resturant then redirect to home
+		if ( $user === null || $role === null || $role !== 'Resturant' ) {
 			redirect( '/' );
-		} else {
-			$type = $this->UserAuth->getUserRole( $user->id );
-			if ( $type === null || $type->type !== 'Resturant' ) {
-				redirect( '/' );
-			}
+			return;
 		}
 
-		$data           = array();
+		$data = array();
+
+		// Get The List of Preference
 		$data['prefer'] = $this->PreferenceModel->getPreferences();
+
+		// View Add Menu item
 		$this->load->view( 'resturant/add_menu_item', $data );
 	}
 
 	/**
+	 * Controller For Add Food menu item to DB.
+	 *
+	 * @return void
 	 */
 	public function add() {
 		$user = $this->session->userdata( 'user' );
-		if ( $user === null ) {
+		$role = $this->session->userdata( 'role' );
+
+		if ( $user === null || $role === null || $role !== 'Resturant' ) {
 			// Show Unauthorized Message if user is not defined
 			$err_msg = 'You don\'t Have permission to access this resource. To Visit Home <a href="' . base_url() . '">Click Here</a>';
 			show_error( $err_msg, 401, 'Unauthorized Access' );
 			return;
 		}
 
+		// Config For File Upload
 		$config['upload_path']   = './uploads/';
 		$config['allowed_types'] = 'gif|jpg|png';
 		$config['encrypt_name']  = true;
 
+		// Load File Upload Library
 		$this->load->library( 'upload', $config );
 
+		// Upload thumbnail if some error occurs show error
 		if ( ! $this->upload->do_upload( 'thumbnail' ) ) {
-				$error = array( 'error' => $this->upload->display_errors() );
-				print_r( $error );
-				// TODO: Handle Error
+			$error = array( 'error' => $this->upload->display_errors() );
+			$this->load->view( 'resturant/add_menu_item', $error );
 		} else {
+			// Load Menu Model.
 			$this->load->model( 'MenuModel' );
 
-			$data               = $this->upload->data();
+			// Get upload Info.
+			$data = $this->upload->data();
+
+			// Create Food Menu item.
 			$menu               = array();
 			$menu['name']       = $this->input->post( 'name' );
 			$menu['price']      = $this->input->post( 'price' );
 			$menu['created_by'] = $user->id;
 			$menu['type']       = $this->input->post( 'preference' );
 			$menu['thumbnail']  = $data['file_name'];
+			// Insert Food Menu Item to DB.
 			$this->MenuModel->create( $menu );
+
+			// Redirect to Home Page.
 			redirect( '/' );
 		}
 	}
 
+	/**
+	 * Controller For Deleting Food menu item.
+	 *
+	 * @return void
+	 */
 	public function delete( $id = -1 ) {
+		// Get user From session.
 		$user = $this->session->userdata( 'user' );
-		if ( $user === null ) {
+		if ( $user === null || $id == -1) {
 			// Show Unauthorized Message if user is not the creator
 			$err_msg = 'You don\'t Have permission to access this resource. To Visit Home <a href="' . base_url() . '">Click Here</a>';
 			show_error( $err_msg, 401, 'Unauthorized Access' );
 			return;
 		}
+		// Load Menu Model.
 		$this->load->model( 'MenuModel' );
+		// Delete item from DB.
 		$this->MenuModel->delete( $id, $user->id );
+		// Redirect to Home.
 		redirect( '/' );
 	}
 
 	/**
+	 * Controller For List of Food Menu item Created by the user.
+	 *
+	 * @param int $pgNo Page Number
+	 *
+	 * @return void
 	 */
 	public function list( $pgNo = 0 ) {
+		// Authenticate user
 		$user = $this->session->userdata( 'user' );
-		
+
 		if ( $user === null ) {
 			// Show Unauthorized Message if user is not the creator
 			$err_msg = 'You don\'t Have permission to access this resource. To Visit Home <a href="' . base_url() . '">Click Here</a>';
@@ -92,16 +129,33 @@ class Menu extends CI_Controller {
 			return;
 		}
 
+		// Load Model
 		$this->load->model( 'MenuModel' );
+		// Set Content Type as JSON
 		header( 'Content-Type: application/json' );
+		// Show Menu items in JSON Format
 		echo json_encode( $this->MenuModel->getMyItems( $user->id, $pgNo ) );
 	}
 
+	/**
+	 * Controller For List of Food Menu items.
+	 *
+	 * @param int $pgNo Page Number
+	 *
+	 * @return void
+	 */
 	public function menu_items( $pgNo = 0 ) {
+		// Load Model
 		$this->load->model( 'MenuModel' );
+		// Set Content type as JSON
 		header( 'Content-Type: application/json' );
+
+		// Get user Object from session
 		$user    = $this->session->userdata( 'user' );
+		// Get user id if user is logged in else make it null
 		$user_id = ( $user !== null ) ? $user->id : null;
+
+		// Print the json array of menu items
 		echo json_encode( $this->MenuModel->getItems( $user_id, $pgNo ) );
 	}
 }
