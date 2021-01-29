4943eb41a027bc8f076aa7e04eaa374bc76fd957
Navonil Das <navneeladas@gmail.com>
2021-01-30 03:33:18 +0530

    Infinite Scroll Resturant Food Menu

--- application/views/resturant/resturant_menu.php
+++ application/views/resturant/resturant_menu.php
@@ -10,28 +10,50 @@ if ( $user === null || $role !== 'Resturant' ) {
 
 
 	<h1 class="m-2">My Menu</h1>
-	<div class="row food-menu m-0 pl-2"></div>
+	<div class="row food-menu m-0 pl-2 mb-2"></div>
 
 	<script>
-	$.get("<?php echo base_url() . 'menu/list'; ?>", function(data, status){
-		const row = $('.food-menu');
-		if(data.length === 0){
-			row.append('<div class="alert alert-primary" role="alert">No Food Items available!</div>');
-		}
-		for(var item of data){
-			row.append(`<div class="col-md-3 mt-3">
-			<div class="card">
-				<img class="card-img-top" src="<?php echo base_url().'uploads/'; ?>${item.thumbnail}" alt="Card image cap">
-				<div class="card-body d-flex">
-					<div class="flex-grow-1">
+	var progress = false, cur_page = 0;
+	function GetList() {
+		if(progress) return;
+		progress = true;
+
+		$.get("<?php echo base_url() . 'menu/list/'; ?>"+cur_page, function(data, status){
+			const row = $('.food-menu');
+			if(data.length === 0 && cur_page === 0){
+				row.append('<div class="alert alert-primary" role="alert">No Food Items available!</div>');
+			}
+			for(var item of data){
+				row.append(`<div class="col-md-3 mt-3">
+				<div class="card">
+					<img class="card-img-top" src="<?php echo base_url().'uploads/'; ?>${item.thumbnail}" alt="Card image cap">
+					<div class="card-body">
 						<h5 class="card-title">${item.name}</h5>
-						<p class="card-text">${item.price} Rs.</p>
+						<div class="d-flex">
+							<div class="flex-grow-1 mt-auto">
+								<p class="card-text">${item.price} Rs.</p>
+							</div>
+							<a href="<?php echo base_url().'menu?id=';?>${item.id}" class="btn btn-primary m-auto"><i class="fa fa-pen"></i></a>
+							<a href="<?php echo base_url().'delete/';?>${item.id}" class="btn btn-danger ml-2 mt-auto mb-auto"><i class="fa fa-trash"></i></a>
+						</div>
 					</div>
-					<a href="<?php echo base_url().'delete/';?>${item.id}" class="btn btn-danger m-auto"><i class="fa fa-trash"></i></a>
-				</div>
-			</div></div>`);
+				</div></div>`);
+			}
+			progress = false;
+			++cur_page;
+		});		
+	}
+	GetList();
+
+	window.addEventListener('scroll',function(event){
+		if(progress) return;
+		var scrollHeight = parseFloat(window.getComputedStyle(document.body).height.replace('px',''));
+		var scrollPos = window.innerHeight + window.scrollY;
+		if(((scrollHeight - 311) >= scrollPos) / scrollHeight == 0){
+			GetList();
 		}
 	});
+
 	</script>
 </body>
 </html>
